5821b905838408102a4766c5077e09ce83f6d0e5
Greg Willard (r3pwn) <wickett06@gmail.com>
2016-10-12 20:37:06 -0400

    sturgeon: initial actual device bringup

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := sturgeon
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/cm/config/common.mk)
@@ -12,6 +12,6 @@ $(call inherit-product, vendor/cm/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sturgeon
 PRODUCT_NAME := cm_sturgeon
-PRODUCT_BRAND := huawei
-PRODUCT_MODEL := sturgeon
-PRODUCT_MANUFACTURER := huawei
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := HUAWEI WATCH
+PRODUCT_MANUFACTURER := HUAWEI
--- device.mk
+++ device.mk
@@ -4,18 +4,10 @@ $(call inherit-product-if-exists, vendor/huawei/sturgeon/sturgeon-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/huawei/sturgeon/overlay
 
 LOCAL_PATH := device/huawei/sturgeon
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-# PRODUCT_COPY_FILES += \
-#     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.sturgeon:root/fstab.sturgeon \
-    $(LOCAL_PATH)/init.recovery.sturgeon.rc:root/init.recovery.sturgeon.rc
+    $(LOCAL_PATH)/rootdir/fstab.sturgeon:root/fstab.sturgeon \
+    $(LOCAL_PATH)/rootdir/init.recovery.sturgeon.rc:root/init.recovery.sturgeon.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
